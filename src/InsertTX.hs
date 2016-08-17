05910abaca9acc28953b72aa60d27e84a1709d15
jamshid <j>
2016-08-17 17:30:38 -0400

    putting correct prvkey in inserttx

--- src/InsertTX.hs
+++ src/InsertTX.hs
@@ -4,9 +4,12 @@ module InsertTX where
 
 import Control.Monad.Logger
 import Control.Monad.Trans.Resource
+import qualified Data.Binary as BN
+import qualified Data.ByteString.Lazy as BL
 import Data.Time.Clock
 import qualified Database.Persist.Postgresql as SQL
 import qualified Network.Haskoin.Crypto as H
+import System.FilePath
 
 import Blockchain.Data.Code
 import Blockchain.Data.RawTransaction
@@ -15,10 +18,17 @@ import Blockchain.Data.TXOrigin
 import Blockchain.EthConf
 
 
-Just prvKey = H.makePrvKey 0xabcd
+--Just prvKey = H.makePrvKey 0xabcd
+
+retrievePrvKey :: FilePath -> IO (Maybe H.PrvKey)
+retrievePrvKey path = do
+    keyBytes <- BL.readFile path
+    return $ H.makePrvKey $ BN.decode keyBytes
+
 
 insertTX::IO ()
 insertTX = do
+  Just prvKey <- retrievePrvKey $ "config" </> "priv"
   theTime <- getCurrentTime
   db <- runNoLoggingT $ SQL.createPostgresqlPool connStr' 20
   tx <- H.withSource H.devURandom $ createContractCreationTX 0 1 1000000 0 (Code "abcd") prvKey
