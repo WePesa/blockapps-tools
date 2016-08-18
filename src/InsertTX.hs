9eb5412f8f3592336384bf9d069724808c3cc171
jamshid <j>
2016-08-18 15:40:31 -0400

    putting a more interesting contract into the inserttx stub (this still should be generized, of course).

--- src/InsertTX.hs
+++ src/InsertTX.hs
@@ -5,6 +5,7 @@ module InsertTX where
 import Control.Monad.Logger
 import Control.Monad.Trans.Resource
 import qualified Data.Binary as BN
+import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import Data.Time.Clock
 import qualified Database.Persist.Postgresql as SQL
@@ -31,7 +32,7 @@ insertTX = do
   Just prvKey <- retrievePrvKey $ "config" </> "priv"
   theTime <- getCurrentTime
   db <- runNoLoggingT $ SQL.createPostgresqlPool connStr' 20
-  tx <- H.withSource H.devURandom $ createContractCreationTX 0 1 1000000 0 (Code "abcd") prvKey
+  tx <- H.withSource H.devURandom $ createContractCreationTX 0 1 1000000 0 (Code $ B.pack [0x60, 0, 0x56]) prvKey
   --let tx = createMessageTX 0 1 1000000 (Address 0xabcd) 1 "" prvKey
   runResourceT $ flip SQL.runSqlPool db $ do
                         SQL.insert (txAndTime2RawTX Direct tx (-1) theTime)
