028db93ce7d99649173e7c068fa903debd6d0c76
kejace <kejace@gmail.com>
2016-09-27 13:40:09 -0400

    Changed to JSON output

--- src/DumpKafkaStateDiff.hs
+++ src/DumpKafkaStateDiff.hs
@@ -8,7 +8,8 @@ import Control.Monad.IO.Class
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Base16 as B16
---import qualified Data.ByteString.Char8 as BC
+--import Data.Text as T
+import qualified Data.ByteString.Char8 as BC
 import Data.Maybe
 import Network.Kafka
 import Network.Kafka.Consumer
@@ -29,7 +30,7 @@ dumpKafkaStateDiff startingBlock = do
       --result :: IO (Data.ByteString.Internal.ByteString)
       result <- fmap (fromMaybe (error "offset out of range")) $ fetchBytesIO (lookupTopic "statediff") offset
       --liftIO $ putStrLn $ unlines $ map (BC.unpack . B16.encode) result
-      liftIO $ putStrLn $ unlines $ fmap show result
+      liftIO $ putStrLn $ unlines $ map BC.unpack result
       doConsume' (offset + fromIntegral (length result))
 
     --theJSON x = (Aeson.decode $ BL.fromStrict $ x :: Maybe Aeson.Value)
