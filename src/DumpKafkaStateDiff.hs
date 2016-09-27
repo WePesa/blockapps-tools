92f3ddc39adaf4d5a6e1d66dabb9c88efe80ef59
kejace <kejace@gmail.com>
2016-09-27 13:13:27 -0400

    Added dumpKafkaStateDiff

--- src/DumpKafkaStateDiff.hs
+++ src/DumpKafkaStateDiff.hs
@@ -5,12 +5,17 @@ module DumpKafkaStateDiff where
 
 import Control.Lens
 import Control.Monad.IO.Class
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Base16 as B16
-import qualified Data.ByteString.Char8 as BC
+--import qualified Data.ByteString.Char8 as BC
 import Data.Maybe
 import Network.Kafka
 import Network.Kafka.Consumer
 import Network.Kafka.Protocol
+
+import qualified Data.Aeson as Aeson
+import Blockchain.Data.StateDiff
 
 import Blockchain.Stream.Raw
 import Blockchain.EthConf
@@ -21,8 +26,11 @@ dumpKafkaStateDiff startingBlock = do
   doConsume' startingBlock
   where
     doConsume' offset = do
+      --result :: IO (Data.ByteString.Internal.ByteString)
       result <- fmap (fromMaybe (error "offset out of range")) $ fetchBytesIO (lookupTopic "statediff") offset
-
-      liftIO $ putStrLn $ unlines $ map (BC.unpack . B16.encode) result
-
+      --liftIO $ putStrLn $ unlines $ map (BC.unpack . B16.encode) result
+      liftIO $ putStrLn $ unlines $ fmap show result
       doConsume' (offset + fromIntegral (length result))
+
+    --theJSON x = (Aeson.decode $ BL.fromStrict $ x :: Maybe Aeson.Value)
+    --readJSON x = fromMaybe "" (Aeson.toJSON $ theJSON x)
